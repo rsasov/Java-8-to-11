5afcde22485bd7c24245042bc63f78a49e621091
Rosen Sasov <rosen@HUAWEI_P20_lite-a2d6b99c4.deltasource>
2018-12-11 11:09:51 +0200

    Add methods for each example and refactor

--- OptionalMethods/src/main/java/OptionalMethods.java
+++ OptionalMethods/src/main/java/OptionalMethods.java
@@ -1,15 +1,27 @@
 import java.util.Optional;
 
 public class OptionalMethods {
-    public static void main(String[] args) {
-        Optional<String> optionalEmpty = Optional.empty();
-        Optional<String> optionalText = Optional.of("Text");
-        //Optional.or()
-        System.out.println(optionalEmpty.or(() -> optionalText));
-        //Optional.ifPresentOrElse
-        optionalText.ifPresentOrElse(
+
+    static void evaluateOptionalOr(Optional<String> opt1, Optional<String> opt2) {
+        System.out.println(opt1.or(() -> opt2));
+    }
+
+    static void evaluateOptionalIfPresentOrElse(Optional<String> opt1) {
+        opt1.ifPresentOrElse(
                 value -> System.out.println("Found: " + value),
                 () -> System.out.println("Not found")
         );
+    }
+
+    public static void main(String[] args) {
+        Optional<String> optionalEmpty = Optional.empty();
+        Optional<String> optionalText = Optional.of("Text");
+
+        //Optional.or()
+        evaluateOptionalOr(optionalEmpty, optionalText);
+
+        //Optional.ifPresentOrElse
+        evaluateOptionalIfPresentOrElse(optionalEmpty);
+
     }
 }
